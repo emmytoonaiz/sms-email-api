eb8f769bee74a3c3fa300bdb8c5b00b78b785ff0
emmytoonaiz <46744255+emmytoonaiz@users.noreply.github.com>
2019-01-27 07:18:34 +0100

    Utilized radical-lib-alert library to implement thread class for sending alerts asynchronously

---
+++ Documents/NetBeansProjects/radical-monitor/src/main/java/radical/lib/monitor/radicalmonitor/RadicalMonitorApplication.java
@@ -0,0 +1,14 @@
+package radical.lib.monitor.radicalmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadicalMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadicalMonitorApplication.class, args);
+	}
+
+}
+
